1055541d5786f13004a476e567e076a4186ba346
Ram Paranjothy <ram.paranjothy@amd.com>
2020-07-06 00:18:07 -0500

    replaced list with set

--- nextword.py
+++ nextword.py
@@ -2,16 +2,17 @@
 #  An attempt to make an index which can help predict the next word.
 
 from collections import defaultdict
-
+import json
 
 def main():
     """
     Given a file, generate a dict with the list of words that followed the current word
     n => n+1
     n+1 => n+2
+    Application: Can be used to predict the next word
     """
 
-    m = defaultdict(list)
+    wordCluster = defaultdict(set)
     with open('data.txt', 'r', encoding='utf-8') as f:
         for line in f.readlines():
             line = line.strip('\n').strip('\t')
@@ -22,10 +23,13 @@ def main():
                 _ = next(wordsInLineNext)
                 wpairs = zip(wordsInLine, wordsInLineNext)
                 for word, nextWord in wpairs:
-                    len(word) and len(nextWord) and m[word].extend([nextWord])
+                    len(word) and len(nextWord) and wordCluster[word.lower()].add(nextWord.lower())
             except Exception as ex:
                 print(f'{ex!s}')
-        print(m)
+        # print(wordCluster)
+        print(f"Clusters Created : {len(wordCluster.keys())}")
+        json.dump(wordCluster,open('outfile.json','w'),default= lambda x: isinstance(x,set) and list(x))
+        
 
 
 if __name__ == "__main__":
